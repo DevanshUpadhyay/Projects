290bd722f258d0a1a23a820cae81d6205509bdfa
DevanshUpadhyay <devanshupadhyay2611@gmail.com>
2023-03-20 15:32:30 +0530

    save

--- controllers/userController.js
+++ controllers/userController.js
@@ -63,8 +63,8 @@ export const logout = catchAsyncErrors(async (req, res, next) => {
       expires: new Date(0),
       httpOnly: true,
       secure: true,
-      sameSite: "Strict",
-      domain: "https://online-video-teaching-streaming-platform.vercel.app",
+      sameSite: "none",
+      // domain: "https://online-video-teaching-streaming-platform.vercel.app",
       // path: "/",
     })
     .json({
