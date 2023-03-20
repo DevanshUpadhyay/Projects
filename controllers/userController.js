f2bf4b053cc4e8e813e055ea6f732c0c57fd5b33
DevanshUpadhyay <devanshupadhyay2611@gmail.com>
2023-03-20 16:59:17 +0530

    new

--- controllers/userController.js
+++ controllers/userController.js
@@ -59,7 +59,7 @@ export const login = catchAsyncErrors(async (req, res, next) => {
 export const logout = catchAsyncErrors(async (req, res, next) => {
   res
     .status(200)
-    .clearCookie("jwt", {
+    .clearCookie("token", {
       expires: new Date(0),
       httpOnly: true,
       secure: true,
