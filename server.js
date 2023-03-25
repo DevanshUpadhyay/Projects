d9f670f3a47fc2435b17fd298a4f831dc50f91b8
DevanshUpadhyay <devanshupadhyay2611@gmail.com>
2023-03-25 19:31:58 +0530

    try

--- server.js
+++ server.js
@@ -25,7 +25,15 @@ nodeCron.schedule("0 0 0 1 * *", async () => {
     console.log(error);
   }
 });
-
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.listen(process.env.PORT, () => {
   console.log(`Server is working on port : ${process.env.PORT}`);
 });
